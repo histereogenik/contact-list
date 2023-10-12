73f935bd1b2cc21592d1a1bba5aafcac8dd62ee5
Humberto Nacif <gigantedeluz@gmail.com>
2023-10-12 13:28:25 -0300

    deleting unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Provider } from 'react-redux'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import EstiloGlobal, { Container } from './styles'
 import Sidebar from './containers/Sidebar'
--- src/components/SavedContact/styles.ts
+++ src/components/SavedContact/styles.ts
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-import variables from '../../styles/variables'
 
 export const ContactContainer = styled.tr`
   td {
